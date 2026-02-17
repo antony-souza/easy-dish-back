117d7edd9f4648a58a51fe47325a2e9997d3a9ba
antony-souza <antonygustavo09@hotmail.com>
2026-02-17 14:14:01 -0300

    middleware de token

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 import { authRoutes } from './modules/auth/auth.routes.js';
+import { ApiTokenMiddleware } from './middlewares/api-token.middleware.js';
 
 export const apiRoutes = Router();
+
+apiRoutes.use(ApiTokenMiddleware);
 
 apiRoutes.use('/auth', authRoutes);
